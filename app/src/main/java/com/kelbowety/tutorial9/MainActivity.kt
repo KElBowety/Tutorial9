37f2cebc68651ba94fb85fcff910a3d1523e2bcb
Karim ElBowety <60698802+KElBowety@users.noreply.github.com>
2023-04-12 07:13:51 +0200

    Initial commit

---
+++ app/src/main/java/com/kelbowety/tutorial9/MainActivity.kt
@@ -0,0 +1,23 @@
+package com.kelbowety.tutorial9
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.auth.ktx.auth
+import com.google.firebase.ktx.Firebase
+import com.kelbowety.tutorial9.databinding.ActivityMainBinding
+
+class MainActivity : AppCompatActivity() {
+    private lateinit var auth: FirebaseAuth
+    private lateinit var binding: ActivityMainBinding
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        val view = binding.root
+        setContentView(view)
+
+        auth = Firebase.auth
+
+    }
+}
